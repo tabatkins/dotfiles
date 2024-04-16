9c4490ccaf80fb4896fff5eae76bd9447a277ad8
Tab Atkins-Bittner <jackalmage@gmail.com>
2024-04-16 15:46:28 -0700

    Prevent ssh-agent.sh from running recursively.

--- bin/sshagent.sh
+++ bin/sshagent.sh
@@ -1,5 +1,7 @@
-{ eval `ssh-agent -s`; } &>/dev/null
-ssh-add -q ~/.ssh/sshkey
-$SHELL
-ssh-agent -k
-exit
+if [ -z "$__IN_SSHAGENT_SUBSHELL" ]; then
+	{ eval `ssh-agent -s`; } &>/dev/null
+	ssh-add -q ~/.ssh/sshkey
+	__IN_SSHAGENT_SUBSHELL=true $SHELL
+	ssh-agent -k
+	exit
+fi;
